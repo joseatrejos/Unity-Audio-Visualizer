2121c7cdf23360dc36d0c6c83df228b8b14430a7
Jose Trejo <joseatrejos@gmail.com>
2020-11-11 01:20:24 -0700

    Audio visualizer with scale

---
+++ Assets/Scripts/AudioVisualizer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class AudioVisualizer : MonoBehaviour
+{
+    [SerializeField] float scaleFactor = 1.0f;
+
+    AudioSource aud;
+
+    float[] spectrum;
+
+    float freq;
+
+    Vector3 initScale;
+
+    void Awake()
+    {
+        spectrum = new float[256];
+        aud = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        initScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        aud.GetOutputData(spectrum, 0);
+        for(int i = 0; i < spectrum.Length; i++)
+        {
+            freq += spectrum[i];
+            // Debug.Log(freq);
+        }
+        transform.localScale = (Vector3.one * freq * scaleFactor) + initScale;
+    }
+}
